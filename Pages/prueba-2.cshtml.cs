e0fef48719c52a21a198808ed9a5d22a09031842
WIL <wilmar.huertas@gmail.com>
2022-09-17 21:17:23 -0500

    inicio del Proyecto

---
+++ Pages/prueba-2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace paginaprueba1.App.Pages
+{
+    public class prueba_2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
