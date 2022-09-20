750afd44b5bd1743ea5bbe9b02f57b4d5e93d02f
wilmanhuertas <wilmar.huertas@gmail.com>
2022-09-19 21:13:30 -0500

    modelo 3 capas agregado

---
+++ Dominio/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dominio
+{
+    public class Persona
+    {
+        public int documento { get; set; }
+        public string nombreCompleto { get; set; }
+        public int telefono { get; set; }
+        public string contraseña { get; set; }
+        public string perfil { get; set; }
+    }
+}
