7747e9aee31f40c984a1621d7086d4f5ed468841
WIL <wilmar.huertas@gmail.com>
2022-09-22 12:52:40 -0500

    modificacion modulo veterinario y formulario

--- obj/Debug/netcoreapp3.1/paginaprueba1.app.RazorTargetAssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/paginaprueba1.app.RazorTargetAssemblyInfo.cs
@@ -12,13 +12,6 @@ using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.CompiledRazorAssemblyApplicationPartFac" +
     "tory, Microsoft.AspNetCore.Mvc.Razor")]
-[assembly: System.Reflection.AssemblyCompanyAttribute("paginaprueba1.app")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyProductAttribute("paginaprueba1.app")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyTitleAttribute("paginaprueba1.app.Views")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
